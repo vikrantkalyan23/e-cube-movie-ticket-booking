a3e01119600d1382cf92a0e5534e7286220dc6aa
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2025-01-15 00:01:27 +0530

    #update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import moviesReducer from './slices/moviesSlice';
+import moviesReducer from './MoviesSlice'; // Adjust based on actual file name
+
 import eventsReducer from './slices/eventsSlice';
 import bookingReducer from './slices/bookingSlice';
 import { setLatestMovies, setUpcomingMovies, setMovieDetails } from './moviesSlice';
